87234f5fd1fc94458ae3983fcecfab49a3d37367
Koosha Mehdipoor <155760765+Koosha-Mehdipoor@users.noreply.github.com>
2026-03-26 14:41:34 +0000

    generating cleaned union file for both green and yellow trips, it will be changed to incremental table later to just append new data

--- dbt/taxi_rides_ny/models/intermediate/int_trips.sql
+++ dbt/taxi_rides_ny/models/intermediate/int_trips.sql
@@ -1,1 +1,52 @@
-SELECT 1
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "pickup_datetime",
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=["vendor_id", "pickup_location_id", "dropoff_location_id"]
+) }}
+
+with trips as (
+
+    select 
+        {{ dbt_utils.generate_surrogate_key([
+            'vendor_id',
+            'pickup_datetime',
+            'pickup_location_id',
+            'service_type'
+        ]) }} as trip_id,
+
+        vendor_id,
+        rate_code_id,
+        pickup_location_id,
+        dropoff_location_id,
+        pickup_datetime,
+        dropoff_datetime,
+        store_and_fwd_flag,
+        passenger_count,
+        trip_distance,
+        trip_type,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        improvement_surcharge,
+        total_amount,
+        coalesce(payment_type, 0) as payment_type,
+        {{ get_payment_type_description('coalesce(payment_type, 0)') }} as payment_type_description,
+        service_type
+
+    from {{ ref('int_trips_unioned') }}
+
+)
+
+select *
+from trips
+qualify row_number() over (
+    partition by trip_id
+    order by dropoff_datetime, dropoff_location_id
+) = 1
