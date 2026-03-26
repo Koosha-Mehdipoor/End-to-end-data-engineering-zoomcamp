82f2b7b84772f68a7bbfd892f1adfcc1d3bb56f9
Koosha Mehdipoor <155760765+Koosha-Mehdipoor@users.noreply.github.com>
2026-03-26 14:41:48 +0000

    creating a dimension for the vendors

--- dbt/taxi_rides_ny/models/marts/dim_vendors.sql
+++ dbt/taxi_rides_ny/models/marts/dim_vendors.sql
@@ -1,1 +1,6 @@
-SELECT 1
+SELECT 
+    DISTINCT vendor_id,
+    --vendor_name created as macro since maybe later would be needed to be used in other models or changed
+    {{ get_vendor_data('vendor_id') }} AS vendor_name
+
+FROM {{ ref('int_trips_unioned') }}
