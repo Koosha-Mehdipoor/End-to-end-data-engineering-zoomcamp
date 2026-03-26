a6cb8092ed3950ec4abbb6eaf6e3e75da10f5685
Koosha Mehdipoor <155760765+Koosha-Mehdipoor@users.noreply.github.com>
2026-03-26 14:40:06 +0000

    macro definition for vendor data definition

---
+++ dbt/taxi_rides_ny/macros/get_vendor_data.sql
@@ -0,0 +1,13 @@
+{% macro get_vendor_data(vendor_id) %}
+    case
+        when {{ vendor_id }} = 1 then 'Creative Mobile Technologies LLC'
+        when {{ vendor_id }} = 2 then 'Curb Mobility LLC'
+        when {{ vendor_id }} = 6 then 'Myle Technologies Inc'
+        when {{ vendor_id }} = 7 then 'Helix'
+        else 'Not Available'
+    end
+{% endmacro %}
+
+
+
+
